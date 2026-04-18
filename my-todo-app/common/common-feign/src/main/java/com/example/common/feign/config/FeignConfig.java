55681afa462a35eecf7ec4f9d995d0e84db3b5b6
zhouy <1815746561@163.com>
2026-04-18 14:42:00 +0800

    fix: 修正 servlet 包名从 javax 迁移到 jakarta (Spring Boot 3.x 兼容)

--- my-todo-app/common/common-feign/src/main/java/com/example/common/feign/config/FeignConfig.java
+++ my-todo-app/common/common-feign/src/main/java/com/example/common/feign/config/FeignConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * Feign 配置类
